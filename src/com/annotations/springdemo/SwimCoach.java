21a29a93ff92875936b50e3ee3297580e23f8e05
June <junetheyi@gmail.com>
2022-07-01 19:45:55 -0400

    defining spring beans with java code. SadForuneService.java, SwimCoach.java, SportConig.java, SwimJavaConfigDemoApp.java

---
+++ src/com/annotations/springdemo/SwimCoach.java
@@ -0,0 +1,23 @@
+package com.annotations.springdemo;
+
+public class SwimCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	public SwimCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+
+		return "Swim 1000m as a warm up";
+	}
+
+	@Override
+	public String getDailyFortune() {
+
+		return fortuneService.getFortune();
+	}
+
+}
